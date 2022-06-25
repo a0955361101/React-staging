982d629de04d3717fd79064ead023ffc5e35d29c
min <a0955361101@gmail.com>
2022-06-25 21:33:05 +0800

    React練習

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// 創建'外殼'組件App
+import React,{Component} from 'react';
+import Hello from './components/Hello/Hello';
+import Welcome from './components/Welcome/Welcome';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>
+       <Hello/>
+       <Welcome/>
+      </div>
+    )
+  }
 }
 
-export default App;
+// 輸出App組件
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+// 引入react 核心庫
 import React from 'react';
+// 引入ReactDOM
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// 引入App組件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 渲染App到頁面
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+</React.StrictMode>
+)
