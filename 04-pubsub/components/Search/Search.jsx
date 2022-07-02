d09a34d53f8624688a267894ea672e388f40563f
min <a0955361101@gmail.com>
2022-07-02 13:43:11 +0800

    搜尋功能

---
+++ 04-pubsub/components/Search/Search.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import PubSub from 'pubsub-js';
+
+
+export default class Search extends Component {
+  
+    search = () =>{
+
+        PubSub.publish('states',{isFirs:false,isLoading:true})
+        // 獲取用戶的輸入
+        const {keyWordElement:{value:keyWord}} = this
+        // 發送請求前通知APP更新狀態
+        
+        
+        // 發送網路請求
+        axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
+            response =>{
+        // 請求成功後通知APP更新狀態
+
+        PubSub.publish('states',{isLoading:false,users:response.data.items})
+                
+            },
+             error=>{PubSub.publish('states',{isLoading:false,err:error.message})}
+        )
+    }
+ 
+
+    render() {
+    return (
+        <section className='jumbotron'>
+           <h3 className='jumbotron-heading'>搜索github用戶</h3>
+           <div>
+            <input ref={c => this.keyWordElement = c} type="text" placeholder='輸入關鍵字點擊搜索'/>&nbsp;
+            <button onClick={this.search}>搜索</button>
+           </div>
+        </section>
+        )
+    }
+}
+
+
